b178e51bd1bbe94c0c98dfb9039c41b542b47be8
Daniel Zauner <daninator.zauner@gmail.com>
2022-11-13 21:11:43 +0100

    add multiplayer menu to pause menu

---
+++ src/main/java/meteordevelopment/meteorclient/mixin/GameMenuScreenMixin.java
@@ -0,0 +1,34 @@
+/*
+ * This file is part of the Meteor Client distribution (https://github.com/MeteorDevelopment/meteor-client).
+ * Copyright (c) Meteor Development.
+ */
+
+package meteordevelopment.meteorclient.mixin;
+
+import net.minecraft.client.gui.screen.GameMenuScreen;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.multiplayer.MultiplayerScreen;
+import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(GameMenuScreen.class)
+public class GameMenuScreenMixin extends Screen {
+    protected GameMenuScreenMixin(Text title) {
+        super(title);
+    }
+
+    @Inject(method = "initWidgets", at = @At("TAIL"))
+    private void OnInitWidgets(CallbackInfo info) {
+        this.addDrawableChild(
+            new ButtonWidget(this.width / 2 - 102,
+                this.height / 4 + 24 + -16 - 24,
+                204,
+                20,
+                Text.translatable("menu.multiplayer"),
+                (button) -> this.client.setScreen(new MultiplayerScreen(this))));
+    }
+}

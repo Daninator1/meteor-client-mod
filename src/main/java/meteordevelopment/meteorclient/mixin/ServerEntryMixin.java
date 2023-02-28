e7931d946e477ed68a6807a6b730f5360b847e3c
Daniel Zauner <daninator.zauner@gmail.com>
2023-02-28 12:39:29 +0100

    fix performance problem in ServerEntryMixin

--- src/main/java/meteordevelopment/meteorclient/mixin/ServerEntryMixin.java
+++ src/main/java/meteordevelopment/meteorclient/mixin/ServerEntryMixin.java
@@ -6,9 +6,12 @@
 package meteordevelopment.meteorclient.mixin;
 
 import meteordevelopment.meteorclient.systems.friends.PlayStatus;
+import net.minecraft.client.gui.screen.multiplayer.MultiplayerScreen;
 import net.minecraft.client.gui.screen.multiplayer.MultiplayerServerListWidget;
+import net.minecraft.client.network.ServerInfo;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
@@ -17,15 +20,21 @@ import java.util.List;
 @Mixin(MultiplayerServerListWidget.ServerEntry.class)
 public class ServerEntryMixin {
 
-    // TODO: fix performance issues
-//    @ModifyVariable(method = "render(Lnet/minecraft/client/util/math/MatrixStack;IIIIIIIZF)V", at = @At("HEAD"), ordinal = 0)
-//    private int onRender(int index) {
-//        return index - GetIndexModifier();
-//    }
+    private int indexModifier;
+
+    @Inject(at = @At("RETURN"), method = "<init>(Lnet/minecraft/client/gui/screen/multiplayer/MultiplayerServerListWidget;Lnet/minecraft/client/gui/screen/multiplayer/MultiplayerScreen;Lnet/minecraft/client/network/ServerInfo;)V")
+    private void init(MultiplayerServerListWidget multiplayerServerListWidget, MultiplayerScreen screen, ServerInfo server, CallbackInfo ci) {
+        this.indexModifier = GetIndexModifier();
+    }
+
+    @ModifyVariable(method = "render(Lnet/minecraft/client/util/math/MatrixStack;IIIIIIIZF)V", at = @At("HEAD"), ordinal = 0)
+    private int onRender(int index) {
+        return index - this.indexModifier;
+    }
 
     @Redirect(method = "keyPressed(III)Z", at = @At(value = "INVOKE", target = "Ljava/util/List;indexOf(Ljava/lang/Object;)I", ordinal = 0))
     private int onKeyPressed(List<MultiplayerServerListWidget.Entry> children, Object object) {
-        return children.indexOf(object) - GetIndexModifier();
+        return children.indexOf(object) - this.indexModifier;
     }
 
     @Inject(method = "keyPressed(III)Z", at = @At(value = "INVOKE", target = "Ljava/util/List;indexOf(Ljava/lang/Object;)I", shift = At.Shift.BY, by = 2), locals = LocalCapture.CAPTURE_FAILHARD, cancellable = true)
@@ -35,12 +44,12 @@ public class ServerEntryMixin {
 
     @Redirect(method = "mouseClicked(DDI)Z", at = @At(value = "INVOKE", target = "Ljava/util/List;indexOf(Ljava/lang/Object;)I", ordinal = 1))
     private int onMouseClicked(List<MultiplayerServerListWidget.Entry> children, Object object) {
-        return children.indexOf(object) - GetIndexModifier();
+        return children.indexOf(object) - this.indexModifier;
     }
 
     @ModifyArg(method = "swapEntries(II)V", at = @At(value = "INVOKE", target = "Ljava/util/List;get(I)Ljava/lang/Object;"), index = 0)
     private int onSwapEntries(int j) {
-        return j + GetIndexModifier();
+        return j + this.indexModifier;
     }
 
     private int GetIndexModifier() {

42c22cf04730a6ab82152fd8b7233be176996f3b
Daniel Zauner <daninator.zauner@gmail.com>
2025-01-30 13:53:50 +0100

    increase max server name length to 1000 characters

--- src/main/java/meteordevelopment/meteorclient/mixin/AddServerScreenMixin.java
+++ src/main/java/meteordevelopment/meteorclient/mixin/AddServerScreenMixin.java
@@ -8,8 +8,11 @@ package meteordevelopment.meteorclient.mixin;
 import net.minecraft.client.gui.screen.multiplayer.AddServerScreen;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.gui.widget.TextFieldWidget;
 import net.minecraft.text.Text;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -17,17 +20,32 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(AddServerScreen.class)
 public class AddServerScreenMixin extends Screen {
 
+    @Shadow
+    private TextFieldWidget serverNameField;
+
     protected AddServerScreenMixin(Text title) {
         super(title);
     }
 
     @Inject(method = "init", at = @At("TAIL"))
-    private void onInit(CallbackInfo info) {
+    private void onInitAddUseLastButton(CallbackInfo info) {
         this.addDrawableChild(
             new ButtonWidget.Builder(Text.literal("Use last"), button -> ((AddServerScreenAccessor) this).getAddressField().setText(this.client != null ? this.client.options.lastServer : ""))
                 .position(this.width / 2 + 104, 106)
                 .size(50, 20)
                 .build()
         );
+    }
+
+    @Inject(
+        method = "init",
+        at = @At(
+            value = "FIELD",
+            target = "Lnet/minecraft/client/gui/screen/multiplayer/AddServerScreen;serverNameField:Lnet/minecraft/client/gui/widget/TextFieldWidget;",
+            opcode = Opcodes.PUTFIELD,
+            shift = At.Shift.AFTER)
+    )
+    private void onInitIncreaseServerNameMaxLength(CallbackInfo info) {
+        this.serverNameField.setMaxLength(1000);
     }
 }
